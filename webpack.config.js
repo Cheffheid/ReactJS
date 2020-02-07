32539a7c55ce600f066f930853bcb1ad1821eb4a
Jeffrey de Wit <jeffrey@webdevstudios.com>
2020-02-07 14:04:03 -0500

    Install and configure webpack packages

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+/* eslint-env node */
+const path = require( 'path' );
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+
+module.exports = {
+	entry: './app/index.js',
+	module: {
+		rules: [
+			{ test: /\.svg$/, use: 'svg-inline-loader' },
+			{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+			{ test: /\.(js)$/, use: 'babel-loader' }
+		]
+	},
+	output: {
+		path: path.resolve( __dirname, 'dist' ),
+		filename: 'index_bundle.js'
+	},
+	plugins: [
+		new HtmlWebpackPlugin()
+	],
+	mode: 'production' === process.env.NODE_ENV ? 'production' : 'development'
+};
