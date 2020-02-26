c988cb7cbb4add6ca48ea0e74eca6d9a8e697a1c
Jeffrey de Wit <jeffrey@webdevstudios.com>
2020-02-26 16:48:46 -0500

    Add first PlayerInput state

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -5,26 +5,26 @@ import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa';
 function Instructions() {
 	return (
 		<div className='instructions-container'>
-			<h1 className='center-text header-lg'>
+			<h2 className='center-text header-lg'>
 				Instructions
-			</h1>
+			</h2>
 			<ol className='container-sm grid center-text battle-instructions'>
 				<li>
-					<h2 className='header-sm'>
+					<h3 className='header-sm'>
 						Enter two Github users.
-					</h2>
+					</h3>
 					<FaUserFriends className='bg-light' color='rgb(255,191,116)' size={140} />
 				</li>
 				<li>
-					<h2 className='header-sm'>
+					<h3 className='header-sm'>
 						Battle
-					</h2>
+					</h3>
 					<FaFighterJet className='bg-light' color='#727272' size={140} />
 				</li>
 				<li>
-					<h2 className='header-sm'>
+					<h3 className='header-sm'>
 						See the winners.
-					</h2>
+					</h3>
 					<FaTrophy className='bg-light' color='rgb(255, 215, 0)' size={140} />
 				</li>
 			</ol>
@@ -92,12 +92,50 @@ PlayerInput.propTypes = {
 }
 
 export default class Battle extends React.Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			playerOne: null,
+			playerTwo: null
+		}
+
+		this.handleSubmit = this.handleSubmit.bind( this );
+	}
+
+	handleSubmit( id, player ) {
+		this.setState( {
+			[id]: player
+		} )
+	}
+
 	render() {
+		const { playerOne, playerTwo } = this.state;
+
 		return (
 			<React.Fragment>
 				<Instructions />
 
-				<PlayerInput label="Label" onSubmit={(value) => console.log( value )} />
+				<div className='players-container'>
+					<h2 className='center-text header-lg'>
+						Players
+					</h2>
+					<div className='row space-around'>
+						{null === playerOne && (
+							<PlayerInput
+								label='Player One'
+								onSubmit={ ( player ) => this.handleSubmit( 'playerOne', player ) }
+							/>
+						)}
+
+						{null === playerTwo && (
+							<PlayerInput
+								label='Player Two'
+								onSubmit={ ( player ) => this.handleSubmit( 'playerTwo', player ) }
+							/>
+						)}
+					</div>
+				</div>
 			</React.Fragment>
 		)
 	}
