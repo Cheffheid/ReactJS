41471dfe0680f186111e46ed2a447bdb4ddbe5e9
Jeffrey de Wit <jeffrey@webdevstudios.com>
2020-02-26 11:56:45 -0500

    Set active button with a class

---
+++ app/components/Popular.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default class Popular extends React.Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			selectedLanguage: 'All'
+		};
+
+		this.updateLanguage = this.updateLanguage.bind(this);
+	}
+
+	updateLanguage( selectedLanguage ) {
+		this.setState( {
+			selectedLanguage
+		} );
+	}
+
+	render() {
+		const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+		return (
+			<ul className="flex-center">
+				{ languages.map( (language) => (
+					<li key={language}>
+						<button
+							className={language === this.state.selectedLanguage ? 'btn-clear nav-link active' : 'btn-clear nav-link' }
+							onClick={() => this.updateLanguage( language )}>
+							{language}
+						</button>
+					</li>
+				) ) }
+			</ul>
+		)
+	}
+}
+
