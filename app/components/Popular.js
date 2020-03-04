006307986725bf8a09fe4cd7107925bd71302e97
Jeffrey de Wit <jeffrey@webdevstudios.com>
2020-03-04 15:15:36 -0500

    Refactor existing cards with new Card component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { fetchPopularRepos } from '../utils/api';
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa';
+import Card from './Card';
 
 function LanguagesNav ({ selected, onUpdateLanguage }) {
 	const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -34,41 +35,37 @@ function ReposGrid( { repos } ) {
 				const { login, avatar_url } = owner;
 
 				return (
-					<li key={html_url} className='card bg-light'>
-						<h2 className='header-lg center-text'>
-							#{index + 1}
-						</h2>
-						<img
-							className='avatar'
-							src={avatar_url}
-							alt=''
-						/>
-						<h3 class='center-text'>
-							<a className='link' href={html_url}>{login}</a>
-						</h3>
-						<ul className='card-list'>
-							<li>
-								<FaUser color='rgb(255, 191, 116)' size={22} />
-								<a href="{`https://github.com/${login}`}">
-									{login}
-								</a>
-							</li>
+					<li key={html_url}>
+						<Card
+							header={`#${index + 1}`}
+							avatar={avatar_url}
+							href={html_url}
+							name={login}
+						>
+							<ul className='card-list'>
+								<li>
+									<FaUser color='rgb(255, 191, 116)' size={22} />
+									<a href="{`https://github.com/${login}`}">
+										{login}
+									</a>
+								</li>
 
-							<li>
-								<FaStar color='rgb(255, 215, 0)' size={22} />
-								{stargazers_count.toLocaleString()} stars
-							</li>
+								<li>
+									<FaStar color='rgb(255, 215, 0)' size={22} />
+									{stargazers_count.toLocaleString()} stars
+								</li>
 
-							<li>
-								<FaCodeBranch color='rgb(129, 195, 245)' size={22} />
-								{stargazers_count.toLocaleString()} forks
-							</li>
+								<li>
+									<FaCodeBranch color='rgb(129, 195, 245)' size={22} />
+									{stargazers_count.toLocaleString()} forks
+								</li>
 
-							<li>
-								<FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
-								{open_issues.toLocaleString()} open issues
-							</li>
-						</ul>
+								<li>
+									<FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
+									{open_issues.toLocaleString()} open issues
+								</li>
+							</ul>
+						</Card>
 					</li>
 				);
 			} ) }
