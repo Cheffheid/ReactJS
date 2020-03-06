fc2dafbdfad4270c7f8ead09186d4824b2600a5c
Jeffrey de Wit <jeffrey@webdevstudios.com>
2020-03-06 15:01:53 -0500

    Add nav to toggle between Popular and Battle

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { ThemeConsumer } from '../contexts/theme';
+
+const activeStyle = {
+	color: 'rgb(187, 46, 31)'
+}
 
 export default function Nav() {
 	return (
 		<ThemeConsumer>
 			{ ( { theme, toggleTheme } ) => (
 				<nav className='row space-between'>
+					<ul className='row nav'>
+						<li>
+							<NavLink
+								to='/'
+								exact
+								activeStyle={activeStyle}
+								className='nav-link'>
+									Popular
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								to='/battle'
+								className='nav-link'
+								activeStyle={activeStyle}>
+									Battle
+							</NavLink>
+						</li>
+					</ul>
 					<button
 						className='btn-clear'
 						onClick={ toggleTheme }
