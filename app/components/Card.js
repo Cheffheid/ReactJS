e49013edb3cfed073667903849e13d3c0ac86c3d
Jeffrey de Wit <jeffrey@webdevstudios.com>
2020-03-06 14:11:44 -0500

    Add ThemeConsumer to Battle/Card components

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import PropTypes, { string } from 'prop-types';
+import { ThemeConsumer } from '../contexts/theme';
 
 export default function Card( { header, subheader, avatar, href, name, children } ) {
 	return (
-		<div className='card bg-light'>
-			<h2 className='header-lg center-text'>
-				{header}
-			</h2>
-			<img
-				className='avatar'
-				src={avatar}
-				alt=''
-			/>
-			{subheader && (
-			<p className='center-text'>
-				{subheader}
-			</p>
-			) }
-			<h3 className='center-text'>
-				<a className='link' href={href}>
-					{name}
-				</a>
-			</h3>
+		<ThemeConsumer>
+		{ ( { theme } ) => (
+			<div className={`card bg-${theme}`}>
+				<h2 className='header-lg center-text'>
+					{header}
+				</h2>
+				<img
+					className='avatar'
+					src={avatar}
+					alt=''
+				/>
+				{subheader && (
+				<p className='center-text'>
+					{subheader}
+				</p>
+				) }
+				<h3 className='center-text'>
+					<a className='link' href={href}>
+						{name}
+					</a>
+				</h3>
 
-			{ children }
-		</div>
+				{ children }
+			</div>
+		) }
+		</ThemeConsumer>
 	)
 };
 
